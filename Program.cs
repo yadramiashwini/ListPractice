db1a6a0fae2448a56d2c9d6f8741e8cd901b876b
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-10 09:40:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ListPractice
+{
+
+    public class ListPractice
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("ashwini"); list.Add("avinash"); list.Add("abhishek"); list.Add("sakshyyyy");
+
+            foreach (var names in list)
+            {
+                Console.WriteLine(names);
+            }
+
+        }
+    }
+}
+            
